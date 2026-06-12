f29f6992af0636403a621454df0ee9d74ede528f
bjzm-mrzdp <1972793439@qq.com>
2026-06-12 15:16:14 +0800

    fix: 取消设置回退 cmpt-picker + wx:if 守卫 + pipe:colon 格式

--- miniprogram_new/pages/admin/meet/add/admin_meet_add.js
+++ miniprogram_new/pages/admin/meet/add/admin_meet_add.js
@@ -16,7 +16,6 @@ Component({
     },
     url(e) { router.url(e, this); },
     bindCateIdSelect(e) { this.setData({ formCateId: e.detail }); if (e.detail != 1) this.setData({ formPhone: '', formPassword: '' }); },
-    bindCancelChange(e) { this.setData({ formCancelSet: e.detail.value }); },
     bindJoinFormsCmpt(e) { this.setData({ formJoinForms: e.detail }); },
 
     bindFormAddSubmit: async function() {
