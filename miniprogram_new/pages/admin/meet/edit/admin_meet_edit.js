5c996955c8c1a2c0319704fd897534c9cbcd5977
bjzm-mrzdp <1972793439@qq.com>
2026-06-12 15:48:43 +0800

    fix: bindCancelSetSelect 防递归 - 空值/同值跳过 setData

--- miniprogram_new/pages/admin/meet/edit/admin_meet_edit.js
+++ miniprogram_new/pages/admin/meet/edit/admin_meet_edit.js
@@ -32,7 +32,7 @@ Component({
     },
     url(e) { router.url(e, this); },
     bindCateIdSelect(e) { this.setData({ formCateId: e.detail }); if (e.detail != 1) this.setData({ formPhone: '', formPassword: '' }); },
-    bindCancelSetSelect(e) { this.setData({ formCancelSet: e.detail }); },
+    bindCancelSetSelect(e) { if (e.detail !== '' && e.detail !== undefined && e.detail !== this.data.formCancelSet) this.setData({ formCancelSet: e.detail }); },
     bindJoinFormsCmpt(e) { this.setData({ formJoinForms: e.detail }); },
 
     bindFormEditSubmit: async function() {
